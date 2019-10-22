929ebc723126b283774d3f5cc59b295346633a88
Felipe Oliveira Souza <felipe.souza@soulisto.com.br>
2019-10-22 13:30:31 -0300

    Warm up challenges

--- HackerRank/Practice/Program.cs
+++ HackerRank/Practice/Program.cs
@@ -12,6 +12,8 @@ namespace Practice
             Console.WriteLine($"\nCounting Valleys: {CountingValleys.Execute(8, "UDDDUDUU")}");
 
             Console.WriteLine($"\nJumping on the Clouds: {JumpingOnTheClouds.Execute(new int[] { 0,0,0,0,1,0 })}");
+
+            Console.WriteLine($"\nRepeated String: {RepeatedString.Execute("gfcaaaecbg", 547602)}"); // 164280
         }
     }
 }
