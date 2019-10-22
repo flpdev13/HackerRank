6857c64d0c541797d61cb60df386a75ea21aaafb
flpdev13 <flp.soul@hotmail.com>
2019-10-21 23:14:25 -0300

    Warm Up challenges

---
+++ HackerRank/Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using Practice.WarmUpChallenges;
+using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"\nSock Merchant: {SockMerchant.Execute(9, new int[] { 10,20,20,10,10,30,50,10,20 })}");
+
+            Console.WriteLine($"\nCounting Valleys: {CountingValleys.Execute(8, "UDDDUDUU")}");
+
+            Console.WriteLine($"\nJumping on the Clouds: {JumpingOnTheClouds.Execute(new int[] { 0,0,0,0,1,0 })}");
+        }
+    }
+}
---
+++ HackerRank/Practice/WarmUpChallenges/JumpingOnTheClouds.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice.WarmUpChallenges
+{
+    public static class JumpingOnTheClouds
+    {
+        public static int Execute(int[] c)
+        {
+            int jump = 0, i = 0;
+
+            while (i < c.Length - 1)
+            {
+                int a = i + 2;
+
+                if (a < c.Length && c[a] == 0)
+                {
+                    ++jump;
+                    i = a;
+                    continue;
+                }
+
+                ++jump;
+                ++i;
+            }
+
+            return jump;
+        }
+    }
+}
