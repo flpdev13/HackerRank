4939bee1c55055c9d754720c49712d34176026e7
Felipe Oliveira Souza <felipe.souza@soulisto.com.br>
2019-10-24 15:39:17 -0300

    Minimum Swaps

--- HackerRank/Practice/Arrays/MinimumSwaps2.cs
+++ HackerRank/Practice/Arrays/MinimumSwaps2.cs
@@ -1,36 +1,37 @@
-﻿namespace Practice.Arrays
+﻿using System.Linq;
+
+namespace Practice.Arrays
 {
     public static class MinimumSwaps2
     {
-        public static int Execute(int[] arr)
+        public static int SelectionSort(int[] arr)
         {
             int swaps = 0;
 
-            // Implementar Quick Sort
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minor = arr[i];
+                int index = -1;
 
-            // Selection Sort (correto porem muito demorado em vetores grandes)
-            //for (int i = 0; i < arr.Length - 1; i++)
-            //{
-            //    int minor = arr[i];
-            //    int index = -1;
+                arr.Where(w => w < minor)
 
-            //    for (int j = i + 1; j < arr.Length; j++)
-            //    {
-            //        if (arr[j] < minor)
-            //        {
-            //            minor = arr[j];
-            //            index = j;
-            //        }
-            //    }
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < minor)
+                    {
+                        minor = arr[j];
+                        index = j;
+                    }
+                }
 
-            //    if (index > -1)
-            //    {
-            //        int aux = arr[i];
-            //        arr[i] = minor;
-            //        arr[index] = aux;
-            //        ++swaps;
-            //    }
-            //}
+                if (index > -1)
+                {
+                    int aux = arr[i];
+                    arr[i] = minor;
+                    arr[index] = aux;
+                    ++swaps;
+                }
+            }
 
             return swaps;
         }
--- HackerRank/PracticeTest/Arrays/MinimumSwaps2Test.cs
+++ HackerRank/PracticeTest/Arrays/MinimumSwaps2Test.cs
@@ -9,9 +9,9 @@ namespace PracticeTest.Arrays
         [InlineData(new int[] { 4, 3, 1, 2 }, 3)]
         [InlineData(new int[] { 2, 3, 4, 1, 5 }, 3)]
         [InlineData(new int[] { 1, 3, 5, 2, 4, 6, 7 }, 3)]
-        public void Test(int[] arr, int expected)
+        public void SelectionSortTest(int[] arr, int expected)
         {
-            Assert.Equal(expected, MinimumSwaps2.Execute(arr));
+            Assert.Equal(expected, MinimumSwaps2.SelectionSort(arr));
         }
     }
 }
