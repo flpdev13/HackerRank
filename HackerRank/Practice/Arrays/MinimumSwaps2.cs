bc0461fd014ba012246a641ffcf1cf4dbc5944bf
flpdev13 <flp.soul@hotmail.com>
2019-10-24 22:08:44 -0300

    Minimum Swaps

--- HackerRank/Practice/Arrays/MinimumSwaps2.cs
+++ HackerRank/Practice/Arrays/MinimumSwaps2.cs
@@ -13,8 +13,6 @@ namespace Practice.Arrays
                 int minor = arr[i];
                 int index = -1;
 
-                arr.Where(w => w < minor)
-
                 for (int j = i + 1; j < arr.Length; j++)
                 {
                     if (arr[j] < minor)
@@ -31,6 +29,38 @@ namespace Practice.Arrays
                     arr[index] = aux;
                     ++swaps;
                 }
+            }
+
+            return swaps;
+        }
+
+        public static int Execute(int[] arr)
+        {
+            int swaps = 0;
+            int index = -1;
+            bool[] testList = new bool[arr.Length];
+
+            var list = arr.Select(s => new { Value = s, Index = ++index })
+                          .OrderBy(o => o.Value)
+                          .ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (testList[i] || list[i].Index == i)
+                    continue;
+
+                int cycle = 0;
+                int j = i;
+
+                while (!testList[j])
+                {
+                    testList[j] = true;
+                    j = list[j].Index;
+                    ++cycle;
+                }
+
+                if (cycle > 0)
+                    swaps += (cycle - 1);
             }
 
             return swaps;
--- HackerRank/PracticeTest/Arrays/MinimumSwaps2Test.cs
+++ HackerRank/PracticeTest/Arrays/MinimumSwaps2Test.cs
@@ -9,9 +9,20 @@ namespace PracticeTest.Arrays
         [InlineData(new int[] { 4, 3, 1, 2 }, 3)]
         [InlineData(new int[] { 2, 3, 4, 1, 5 }, 3)]
         [InlineData(new int[] { 1, 3, 5, 2, 4, 6, 7 }, 3)]
+        [InlineData(new int[] { 2, 31, 1, 38, 29, 5, 44, 6, 12, 18, 39, 9, 48, 49, 13, 11, 7, 27, 14, 33, 50, 21, 46, 23, 15, 26, 8, 47, 40, 3, 32, 22, 34, 42, 16, 41, 24, 10, 4, 28, 36, 30, 37, 35, 20, 17, 45, 43, 25, 19 }, 46)]
         public void SelectionSortTest(int[] arr, int expected)
         {
             Assert.Equal(expected, MinimumSwaps2.SelectionSort(arr));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4, 3, 1, 2 }, 3)]
+        [InlineData(new int[] { 2, 3, 4, 1, 5 }, 3)]
+        [InlineData(new int[] { 1, 3, 5, 2, 4, 6, 7 }, 3)]
+        [InlineData(new int[] { 2,31,1,38,29,5,44,6,12,18,39,9,48,49,13,11,7,27,14,33,50,21,46,23,15,26,8,47,40,3,32,22,34,42,16,41,24,10,4,28,36,30,37,35,20,17,45,43,25,19 }, 46)]
+        public void Test(int[] arr, int expected)
+        {
+            Assert.Equal(expected, MinimumSwaps2.Execute(arr));
         }
     }
 }
